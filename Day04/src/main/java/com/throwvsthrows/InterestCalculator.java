93496898ca7d3473083feb8505c220390cf4da95
Unaiz Siddiqui <unaiz4321@gmail.com>
2025-02-14 23:49:59 +0530

    [Unaiz Siddiqui] Day04 Interest Calculator solution

---
+++ Day04/src/main/java/com/throwvsthrows/InterestCalculator.java
@@ -0,0 +1,11 @@
+package com.throwvsthrows;
+
+public class InterestCalculator {
+
+    public static double calculateInterest(double amount, double rate, int years) throws IllegalArgumentException {
+        if (amount < 0 || rate < 0) {
+            throw new IllegalArgumentException("Amount and rate must be positive");
+        }
+        return (amount * rate * years) / 100;
+    }
+}
