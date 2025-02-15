cd96d3cd2abd213c9579afea50befd8f09abe4dd
Unaiz Siddiqui <unaiz4321@gmail.com>
2025-02-15 20:18:54 +0530

    [Unaiz Siddiqui]  Extract All Email Addresses from a Text solution Day05(regex)

---
+++ Day05/regex/src/main/java/com/extractionproblems/ExtractCapitalizedWords.java
@@ -0,0 +1,33 @@
+package com.extractionproblems;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractCapitalizedWords {
+
+    //method to extract capitalized words
+    public static void extractCapitalizedWords(String text) {
+
+        //regex pattern to extract capital words
+        String regex = "\\b[A-Z][a-z]*\\b";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        //displaying the result
+        while (matcher.find()) {
+            System.out.print(matcher.group() + ", ");
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        //demo text
+        String text = "The Eiffel Tower is in Paris and the Statue of Liberty is in New York.";
+
+        //method call
+        extractCapitalizedWords(text);
+
+    }
+}
