47227dae5dc05a2d9a3632900d690e52153e04c2
Unaiz Siddiqui <unaiz4321@gmail.com>
2025-02-17 01:28:50 +0530

    [Unaiz Siddiqui] ListManager class and addElement, removeElement, getSize methods and testing of methods - Day05(Junit testing)

---
+++ Day05/JUnit/src/main/java/com/ListManager.java
@@ -0,0 +1,23 @@
+package com;
+
+import java.util.List;
+
+public class ListManager {
+
+    //    method to add element in the list
+    public static List<Integer> addElement(List<Integer> list, int element) {
+        list.add(element);
+        return list;
+    }
+
+    //    method to return element in the list
+    public static List<Integer> removeElement(List<Integer> list, int element) {
+        list.remove(element);
+        return list;
+    }
+
+    //    method to get the size of list
+    public static int getSize(List<Integer> list) {
+        return list.size();
+    }
+}
