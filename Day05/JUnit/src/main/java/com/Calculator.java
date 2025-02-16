c23c6fa6058f9fc0bceff8a32ad8f7146e403f7d
Unaiz Siddiqui <unaiz4321@gmail.com>
2025-02-16 21:07:28 +0530

    [Unaiz Siddiqui] Calculator and CalculatorTesting class  Day05(Junit testing)

---
+++ Day05/JUnit/src/main/java/com/Calculator.java
@@ -0,0 +1,27 @@
+package com;
+
+public class Calculator {
+    //addition
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    //subtraction
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    //multiplication
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    //division
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+    //main method
+    public static void main(String[] args) {
+    }
+}
