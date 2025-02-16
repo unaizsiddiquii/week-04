00989ca138afb7b15cb02af57fa942c4cca6799c
Unaiz Siddiqui <unaiz4321@gmail.com>
2025-02-17 02:36:30 +0530

    [Unaiz Siddiqui] database connection BeforeEach AfterEach day05(JUnit)

---
+++ Day05/JUnit/src/main/java/com/DatabaseConnection.java
@@ -0,0 +1,20 @@
+package com;
+
+public class DatabaseConnection {
+
+    private boolean connected;
+
+    public void connect() {
+        connected = true;
+        System.out.println("Database connected");
+    }
+
+    public void disconnect() {
+        connected = false;
+        System.out.println("Database disconnected");
+    }
+
+    public boolean isConnected() {
+        return connected;
+    }
+}
